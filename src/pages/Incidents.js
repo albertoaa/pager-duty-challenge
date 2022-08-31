34cd55fbb3073704d099006c9f8563cb5cf7daf4
Alberto Aragon Alvarez <ronin8503@gmail.com>
2022-08-31 12:02:20 +0100

    basic list of incidents

--- src/pages/Incidents.js
+++ src/pages/Incidents.js
@@ -1,10 +1,34 @@
 import Header from '../components/Header';
 
-const Incidents = () => {
+const Incidents = ({ incidents }) => {
   return (
     <div className='container'>
       <Header />
       <h1>These are your most recent incidents</h1>
+      <div className='container-fluid'>
+        <table class='table'>
+          <thead>
+            <tr>
+              <th scope='col'>ID</th>
+              <th scope='col'>Title</th>
+              <th scope='col'>Summary</th>
+              <th scope='col'>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {incidents.map((incident) => {
+              return (
+                <tr>
+                  <th scope='row'>{incident.id}</th>
+                  <td>{incident.title}</td>
+                  <td>{incident.summary}</td>
+                  <td>{incident.status}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
