dbe412fda7c5ad3d8e76f58ab9f316d1a8e11c66
Alberto Aragon Alvarez <ronin8503@gmail.com>
2022-08-31 15:09:09 +0100

    fixing warning class for className

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,17 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <nav class='navbar navbar-expand-lg'>
-      <div class='container-fluid'>
-        <a class='navbar-brand' href='#'>
+    <nav className='navbar navbar-expand-lg'>
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='#'>
           <img
             src={process.env.PUBLIC_URL + 'images/logo.png'}
             alt='Logo'
-            class='logo-image'
+            className='logo-image'
           />
         </a>
         <button
-          class='navbar-toggler'
+          className='navbar-toggler'
           type='button'
           data-bs-toggle='collapse'
           data-bs-target='#navbarSupportedContent'
@@ -20,29 +20,29 @@ const Header = () => {
           aria-expanded='false'
           aria-label='Toggle navigation'
         >
-          <span class='navbar-toggler-icon'></span>
+          <span className='navbar-toggler-icon'></span>
         </button>
-        <div class='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li class='nav-item'>
-              <a class='nav-link active' aria-current='page' href='/'>
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+            <li className='nav-item'>
+              <a className='nav-link active' aria-current='page' href='/'>
                 Home
               </a>
             </li>
-            <li class='nav-item'>
-              <a class='nav-link' href='/incidents'>
+            <li className='nav-item'>
+              <a className='nav-link' href='/incidents'>
                 Incidents
               </a>
             </li>
           </ul>
-          <form class='d-flex' role='search'>
+          <form className='d-flex' role='search'>
             <input
-              class='form-control me-2'
+              className='form-control me-2'
               type='search'
               placeholder='Search'
               aria-label='Search'
             />
-            <button class='btn btn-outline-success' type='submit'>
+            <button className='btn btn-outline-success' type='submit'>
               Search
             </button>
           </form>
