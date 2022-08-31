11fd8a8e0f5765a02972250c97c794c11d2dde17
Alberto Aragon Alvarez <ronin8503@gmail.com>
2022-08-31 11:29:08 +0100

    adding basic navigation to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
 import { getItems } from './api/index';
+import { Routes, Route, Link } from 'react-router-dom';
 
-function App() {
+const App = () => {
   useEffect(() => {
     getItems('/incidents', {
       limit: 10,
@@ -15,22 +15,40 @@ function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to React Router</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/incidents' element={<Incidents />} />
+      </Routes>
     </div>
   );
-}
+};
 
+const Home = () => {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to='/incidents'>Incidents</Link>
+      </nav>
+    </>
+  );
+};
+
+const Incidents = () => {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to='/'>Home</Link>
+      </nav>
+    </>
+  );
+};
 export default App;
